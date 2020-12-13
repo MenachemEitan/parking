d2976d51341dee25f5ab0cf122e6cd63d67b09e9
Moshe Kessler <moshe@kessler.tech>
2020-12-13 14:48:24 +0100

    add readme file

--- parking-app/src/app/components/navbar/navbar.component.ts
+++ parking-app/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../../services/auth.service';
+import { Client } from '../../models/client.interface';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+// import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn: boolean;
+  loggedInUser: string;
+  showRegister: boolean;
+  constructor(
+    // private router: Router,
+    private flash: FlashMessagesService,
+    private authSvc: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.authSvc.getAuth().subscribe(auth => {
+      if (auth) {
+        this.isLoggedIn = true;
+        this.loggedInUser = auth.email;
+      } else {
+        this.isLoggedIn = false;
+      }
+    });
+  }
+
+  onLogoutClick(): void {
+    this.authSvc.logout();
+    this.flash.show('Logged out successfuly', {
+      cssClass: 'alert-success',
+      timeout: 4000
+    });
+    location.reload();
   }
 
 }
---
+++ readme.md
@@ -0,0 +1,27 @@
+# User Flow
+
+1. Login / Registration.  
+
+2. Parking request form.
+
+* 2.1. No place to park.
+Have parking place -> driver and car form.
+
+ * 2.2 Success / failure message.  
+
+3. message control 
+
+when a user parked their car, they get a message 5 minutes before timeout.
+when a user approved a parking place, they get a notification of their parking details.
+
+# Admin Flow
+
+1. Login  
+
+2. Dashboard - 
+
+* add parking field
+* add parking place
+* claim parking offenses
+* parking situation
+
